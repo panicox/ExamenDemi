626d253e70b7ff2a5728f6a9feba8aa6cc84aa1f
Cristian Encinas Gayo <panicoyalice@gmail.com>
2016-12-20 19:41:24 +0100

    Scripts personaje 1 y 2
    No sé por qué me dan error los scripts :(

---
+++ Assets/Scripts/player1.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class player1 : MonoBehaviour {
+	public float velocidad = 5f;
+	public float power = 1f;
+	private Rigidbody2D rb;
+
+	void start (){
+
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("right")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetKeyUp ("right")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("up")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetKeyUp ("up")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("left")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetKeyUp ("left")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("down")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		if (Input.GetKeyUp ("down")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+	}
+}
---
+++ Assets/Scripts/player2.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+	public class player2 : MonoBehaviour {
+	public float velocidad = 5f;
+	public float power = 1f;
+	private Rigidbody2D rb;
+
+	void start (){
+		
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey ("d")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetKeyUp ("d")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("w")) {
+			rb.velocity = new Vector2 (velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		if (Input.GetKeyUp ("w")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("a")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		if (Input.GetKeyUp ("a")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+
+		if (Input.GetKey ("s")) {
+			rb.velocity = new Vector2 (-velocidad * power, rb.velocity.y);
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+		if (Input.GetKeyUp ("s")) {
+			rb.velocity = new Vector2 (0, rb.velocity.y);
+		}
+	}
+}
